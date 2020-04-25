1259b04dc9075c301f1207b2f38f92eae7e64436
cmf2196 <cmf2196@columbia.edu>
2020-04-25 12:19:58 -0400

    finished rough draft of hill_climber.py

--- hill_climber.py
+++ hill_climber.py
@@ -8,9 +8,25 @@ class hill_climber():
 		self.robot = robot
 
 
-	def mutate():
-		pass
+	def mutate(self , genome):
+		"""
+		This makes a mutation (very small random change) The a single robot's genome
+		Arg:
+			genome: a nxm numpy array
+		"""
 
+
+		num_rows = genome.shape[0]
+		num_cols = genome.shape[1]
+
+		row = np.random.randint(num_rows)			# get the indices for rows , columns
+		col = np.random.randint(num_cols)			#              "  "
+		adjustment = np.random.uniform(0.9 , 1.1)		# select random value to multiply element with
+		old_val = genome[row , col]						
+		genome[row , col] = genome[row , col] * adjustment			# perform multiplication
+
+		return [row , col] , old_val 					# return location of mutation and old value
+														#  (In case we need to change back)
 
 
 	def run_algorithm():
@@ -24,29 +40,20 @@ class hill_climber():
 		num_climb = self.num_iterations
 
 		hc_fit = np.zeros(num_climb+1)								# create an array to store the learning curve data
-		# keep track of progress
-		best_fit = 0												# init the best fit as zero
-		best_array = np.empty(8)									# init a random array as the best robot
-		# make a random robot	
+
 
 		robot.build_genome()										# build the genome
 
 		best_fit = calc_fitness()					# get the fitness of the first robot
+		best_array = robot.genome 					# initialize the best array to keep track
 		hc_fit[0] = best_fit						# place it as the first value in the learning curve array
 
 		for i in range(1 ,num_climb + 1):			# perform mutations equal to num_Climb
 			print('mut' , i)						
-			mut_loc , old_val = robot.mutate_genome()		# Mutation:  Keep track of mut location and previous vals
-													# rebuild the robot with the new (mutated) genome
-			robot.remove_old()						# remove old springs and masses
-			robot.set_tetr( np.array((0 , 0 , 0)) , np.array((0 , 0 , 0)))		# set the robot to pos(0 , 0 , 0)
-			robot.build_robot()						# builds the robot
-													
-			mass_list = robot.mass_list				# center the robot
-			com = robot.get_com(mass_list)			# 		....
-			robot.center_object(com)				# 		....
+			mut_loc , old_val = self.mutate(robot.genome)		# Mutation:  Keep track of mut location and previous vals
 
 			fit_new = calc_fitness( robot , sim)	# calculate new fitness
+			
 			if fit_new > best_fit:					# if better update the top fitness, and top performing genome
 				best_fit = fit_new					
 				best_array = np.copy(robot.genome)
