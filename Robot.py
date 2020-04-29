f5b99071b8fa0ef955dd65ae45902bf771713815
Jordan Finn <jordanfinn@Jordans-MacBook-Air.local>
2020-04-29 10:48:06 -0400

    added functionality for symetrical considerations. Ready to merge

--- Robot.py
+++ Robot.py
@@ -9,10 +9,11 @@ and methods to change those fields
 import numpy as np 
 
 class Robot:
-    def __init__(self, urdf_path, parameters=() , genome = []):
+    def __init__(self, urdf_path, parameters=() , genome = [] , num_joints = 12):
         self.urdf_path = urdf_path
         self.parameters = parameters
         self.genome = genome
+        self.num_joints = num_joints
         self.fitness = 0  # default value, determined in simulation later
 
     def get_urdf_path(self):
@@ -41,12 +42,13 @@ class Robot:
         mutation = np.random.uniform(0.9 , 1.1)
         self.genome[index_1][index_2] = old_val * mutation          # update the genome
 
-        return loc , old_val          # important for negating a mutation
+        return loc , old_val      # important for negating a mutation
 
 
-    def randomize_genome(self):
-        # Use random number generator (with some constraints) to create
-        # random parameters
+    def randomize_genome(self , symmetric = False):
+        # creates a random list of lists as the robot's genome 
+        # this uses some predefined constraints (hardcoded below for now)
+        # the boolean value 'symmetric' is used to determine if we build half or all of the genome
 
         # for now the starting position and joit limit are hardcoded: (This could be read in later )  
         starting_pos = [0 , -1 * np.pi / (4.0) ,  -1 * np.pi / (2.0) , 0 , np.pi / (4.0), np.pi / (2.0), 0 ,  -1 * np.pi / (4.0),
@@ -57,28 +59,54 @@ class Robot:
         limit_3 = (np.radians(-135) , np.radians(135) )      # joint limits for the elbow motor
 
         g = []                     
-        for i in range(12):        # build the genome
-           
-            viable = False
-            while viable == False:
-                # set the correct joint limits
-                if i % 3 == 0:
-                    lower = limit_1[0]
-                    upper = limit_1[1]
-                elif i % 3 == 1:
-                    lower = limit_2[0]
-                    upper = limit_2[1]
-                else:
-                    lower = limit_3[0]
-                    upper = limit_3[1]
+        
+        if symmetric == False: 
+            for i in range(self.num_joints):        # build the genome
+                viable = False
+                while viable == False:
+                    # set the correct joint limits
+                    if i % 3 == 0:
+                        lower = limit_1[0]
+                        upper = limit_1[1]
+                    elif i % 3 == 1:
+                        lower = limit_2[0]
+                        upper = limit_2[1]
+                    else:
+                        lower = limit_3[0]
+                        upper = limit_3[1]
 
-                c = np.random.uniform(lower , upper)  # get a c value within the joint limits
-                a = min(c - lower  , upper - c)  # get an A value according to the closets joint limit
+                    c = np.random.uniform(lower , upper)  # get a c value within the joint limits
+                    a = min(c - lower  , upper - c)       # get an A value according to the closets joint limit
 
-                if starting_pos[i] <= c + a and starting_pos[i] >= c - a : 
-                    g += [[a , c]]        # if the joint starting position is within the possible values
-                    viable = True       #
+                    if starting_pos[i] <= c + a and starting_pos[i] >= c - a : 
+                        g += [[a , c]]                    # if the joint starting position is within the possible values
+                        viable = True      
+        
+        else: 
+            half = self.num_joints / 2   # assuming symmetry 
+            for i in range(half):        # build the genome
+                viable = False
+                while viable == False:
+                    # set the correct joint limits
+                    if i % 3 == 0:
+                        lower = limit_1[0]
+                        upper = limit_1[1]
+                    elif i % 3 == 1:
+                        lower = limit_2[0]
+                        upper = limit_2[1]
+                    else:
+                        lower = limit_3[0]
+                        upper = limit_3[1]
+
+                    c = np.random.uniform(lower , upper)    # get a c value within the joint limits
+                    a = min(c - lower  , upper - c)         # get an A value according to the closets joint limit
+
+                    if starting_pos[i] <= c + a and starting_pos[i] >= c - a : 
+                        g += [[a , c]]                      # if the joint starting position is within the possible values
+                        viable = True  
+
         self.genome = g
+
 
     def get_fitness(self):
         return self.fitness
