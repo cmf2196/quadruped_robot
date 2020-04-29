6630e7a5ed2e9f1d9da6be4f0cdf3c49e6ea07ef
Jordan Finn <jordanfinn@Jordans-MacBook-Air.local>
2020-04-29 12:16:19 -0400

    finished prepping random search and hill climber algorithms. started working on beam search

--- hill_climber.py
+++ hill_climber.py
@@ -6,9 +6,10 @@ This Class will run the hill climber algorithm. It requires the related robot an
 """
 
 import numpy as np 
-# TO DO - import the robot and simulator classes
+from Robot import Robot
+# TO DO - import the  simulator class
 
-class hill_climber():
+class Hill_Climber():
 
 	def __init__(self, num_iterations , robot , simulator):
 		self.num_iterations = num_iterations
@@ -16,7 +17,7 @@ class hill_climber():
 		self.simulator = simulator
 
 
-	def run_algorithm():
+	def run_algorithm(self):
 
 		"""
 		* this function performs a parametric optimization hill climber algorithm on a single robot.
@@ -28,27 +29,41 @@ class hill_climber():
 		num_climb = self.num_iterations
 
 		hc_fit = np.zeros(num_climb)				# create an array to store the learning curve data
-
 		robot.randomize_genome()					# build the genome
-
-		best_fit = simulator.calc_fitness(robot)	# get the fitness of the first robot
+		simulator.calc_fitness(robot)				# get the fitness of the first robot
+		best_fit = robot.get_fitness()				
 		best_genome = robot.genome 					# initialize the best array to keep track
 		hc_fit[0] = best_fit						# place it as the first value in the learning curve array
 
 		for i in range(1 ,num_climb):		    	# perform mutations equal to num_Climb
 			print('mut' , i)						
-			mut_loc , old_val = robot.mutate_genome()		# Mutation:  Keep track of mut location and previous vals
-
-			fit_new = simulator.calc_fitness( robot )	# calculate new fitness
+			mut_loc , old_val = robot.mutate_genome()	# Mutation:  Keep track of mut location and previous vals
+			simulator.calc_fitness(robot)				# get the fitness of the first robot
+			fit_new = robot.get_fitness()	
 			
 			if fit_new > best_fit:					# if better update the top fitness, and top performing genome
 				best_fit = fit_new					
 				best_genome = robot.genome.copy()
 
 			else:									# if worse, revert the genome back to its previous state. 
-				robot.genome[mut_loc[0]][mut_loc[1]] = old_val				# only need to adjust genome
-			hc_fit[i] = best_fit											# robot is rebuilt prior to fitness calculation each time
+				robot.genome[mut_loc[0]][mut_loc[1]] = old_val		# only need to adjust genome
+			hc_fit[i] = best_fit									# robot is rebuilt prior to fitness calculation each time
 			
 		np.savetxt("hc_genome.csv", best_genome, delimiter=",")
 		np.savetxt("hc_learning.csv", hc_fit, delimiter=",")
+
+
+class Simulator():
+    # Place holder class: it returns a random fitness
+    # TO DO - import the actual simulator class
+    def calc_fitness(self, robot):
+        fit = np.random.uniform(0 , 10000)
+        robot.fitness = fit
+
+if __name__ == '__main__':
+    s = Simulator()
+    r = Robot('path_to_urdf' )
+
+    alg = Hill_Climber(100 , r , s)
+    alg.run_algorithm()
 
--- random_search.py
+++ random_search.py
@@ -1,8 +1,15 @@
+"""
+Connor Finn, Joshua Katz
+4/29/2020
+
+This file is used to run the random search algorithm on our robot within the bullet simulator
+"""
+
 import numpy as np 
+from Robot import Robot
+# TO DO - Need to import in the simulator class
 
-# TO DO - Need to import in the robot and simulator classes
-
-class random_search():
+class Random_Search():
     def __init__(self, num_iterations , robot , simulator):
         self.num_iterations = num_iterations
         self.robot = robot
@@ -10,35 +17,46 @@ class random_search():
 
 
     def run_algorithm(self ):
-
-        """ 
-        * this function will perform a random search for the best robot
-        * this is done by generating a new random robot, comparing it to the current best, 
-        * and keeping it only if it achieves higher fitness
-        """     
+       # This will perform a random search algorithm
+       # It will return two csv files:
+       #   1. The optimal genome in rs_genome.csv
+       #   2. The learning curve in rs_learning.csv
 
         robot = self.robot
         simulator = self.simulator
         num_search = self.num_iterations
-
         rs_fit = np.zeros(num_search)               # learning curve array
         best_fit = 0                                # best fitness place holder
-        best_array = None                           # top performing genome place holder
+        best_genome = None                           # top performing genome place holder
         
         for i in range(num_search):                 # do the random search  
             print('counter: ', i)
-
             robot.randomize_genome()                # build the robot's genome
-            simulator.calc_fitness()                # evaluate the robot's fitness
+            simulator.calc_fitness(robot)           # evaluate the robot's fitness
             fitness = robot.get_fitness()
 
             if fitness > best_fit:                  # if the fitness is better than current best
                 best_fit = fitness                  # update placeholders
-                best_array = robot.genome.copy()    #       ....
+                best_genome = robot.genome.copy()    #       ....
 
             rs_fit[i] = best_fit                    # add the best fit to the learning curve
 
-        np.savetxt("rs_genome.csv", best_array, delimiter=",")
+        np.savetxt("rs_genome.csv", best_genome, delimiter=",")
         np.savetxt("rs_learning.csv", rs_fit, delimiter=",")
+
+
+class Simulator():
+    # Place holder class: it returns a random fitness
+    # TO DO - import the actual simulator class
+    def calc_fitness(self, robot):
+        fit = np.random.uniform(0 , 10000)
+        robot.fitness = fit
+
+if __name__ == '__main__':
+    s = Simulator()
+    r = Robot('path_to_urdf' )
+
+    alg = Random_Search(100 , r , s)
+    alg.run_algorithm()
 
 
