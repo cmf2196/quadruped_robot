b7d7135781cffd8d2743fa1e69bebbcbfff0b6f3
joshk <jhk2192@columbia.edu>
2020-05-04 01:47:27 -0400

    Added title, labels, and legend to learning curve plot.

--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ def read_csv(file_path):
 
 
 # all paths from which to collect data
-paths = ["data/ea_2_100k_learning.csv", "data/hc_learning_curve_100k.csv"]
+paths = ["data/ea_2_100k_learning.csv", "data/hc_learning_curve_100k.csv", "random_30k_curve.csv"]
 
 # collect the data in arrays
 all_data = []
@@ -27,7 +27,12 @@ for path in paths:
 #print(all_data)
 ea = np.asarray(all_data[0])
 hc = np.asarray(all_data[1])
+ra = np.asarray(all_data[2])
 x = np.arange(100000)
 
-plt.plot(x, ea, 'r--', x , hc , 'b--')
+plt.plot(x, ea, 'r--', x , hc , 'b--', ra, "g--")
+plt.xlabel("Evaluations")
+plt.ylabel("Fitness")
+plt.title("Learning Curves")
+plt.legend(["Evolutionary Algorithm", "Hill Climber", "Random"])
 plt.show()
