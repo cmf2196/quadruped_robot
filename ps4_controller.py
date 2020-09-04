f0e3eccbc069951e6f3115f3f03a091796dbc03b
cmf2196 <cmf2196@columbia.edu>
2020-09-04 12:49:28 -0400

    bug fix

--- ps4_controller.py
+++ ps4_controller.py
@@ -330,6 +330,6 @@ class MyEventDefinition(Event):
 
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False, event_definition=MyEventDefinition)
-#controller.debug = True  # you will see raw data stream for any button press, even if that button is not mapped
+controller.debug = True  # you will see raw data stream for any button press, even if that button is not mapped
 # you can start listening before controller is paired, as long as you pair it within the timeout window
 controller.listen(timeout=60)
