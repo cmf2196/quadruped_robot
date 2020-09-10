49838dd2cf9cde082043c0610077c5f3ea7334a3
Connor Finn <connor.finn1200@gmail.com>
2020-09-10 07:13:19 -0400

    sensor is configured, time to build imu class

--- bno055_simpletest.py
+++ bno055_simpletest.py
@@ -1,29 +1,56 @@
+'''
+Author: Connor Finn
+Date: 9/ 20/ 2020
+
+
+For our purposes it will only be feasible to calibrate the gyroscope
+The accelerometer and magnetometer required controlled motion in order
+to calibrate.
+
+This sensor does not 'play well' with UART; however, the Pi's capabilities 
+don't allow I2C connection due to something called clock stretching.To remedy
+this, we will catch the RuntimeError (UART read error: 7)
+
+
+'''
+
+
+
+
+
+
+
 import time
-import board
-import busio
 import adafruit_bno055
-
-# Use these lines for I2C
-#i2c = busio.I2C(board.SCL, board.SDA)
-#sensor = adafruit_bno055.BNO055_I2C(i2c)
-
-# User these lines for UART
-#uart = busio.UART(board.TX, board.RX)
 import serial
+
+
+
+
+
 uart = serial.Serial('/dev/serial0', baudrate=9600, timeout=10)
-#uart = serial.Serial('/dev/serial0')
-#sensor = bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
 sensor = adafruit_bno055.BNO055_UART(uart)
 
-while True:
 
-    print("Temperature: {} degrees C".format(sensor.temperature))
-    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
-    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
-    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
-    print("Euler angle: {}".format(sensor.euler))
-    print("Quaternion: {}".format(sensor.quaternion))
-    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
-    print("Gravity (m/s^2): {}".format(sensor.gravity))
-    print()
-    time.sleep(1)
+
+if __name__ == "__main__":
+
+   while True:
+
+       try:
+          print("Temperature: {} degrees C".format(sensor.temperature))
+          print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+          print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+          print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+          print("Euler angle: {}".format(sensor.euler))
+          print("Quaternion: {}".format(sensor.quaternion))
+          print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+          print("Gravity (m/s^2): {}".format(sensor.gravity))
+          print("calibration " , sensor.calibration_status)
+          print()
+    
+       except RuntimeError:
+          print('missed one')
+          pass
+
+       time.sleep(1)
