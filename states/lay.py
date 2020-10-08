4ce753d01c9ecf23df8294a222f607d571295d6e
joshk <jhk2192@columbia.edu>
2020-10-08 15:31:16 -0400

    Merge branch 'master' of C:\Users\joshk\PycharmProjects\quadruped_robot with conflicts.

--- states/lay.py
+++ states/lay.py
@@ -8,9 +8,17 @@ class Lay(State):
 	def __init__(self):
 		pass
 
+	def enter(self, robot):
+		# turn off motors
+	    robot.motor_controller.turn_off_all_motors()
+
 
 	def exit(self , new_state):
 		# new_state is a string (the name of the new state to be entered)
+
+		# upon exit, turn motors back on
+
+
 		return new_state
 
 
@@ -18,6 +26,7 @@ class Lay(State):
 		# all we need to do here is wait to stand up
 		#print('now in lay')
 		if controller_state[4] == 1:
+			robot.motor_controller.turn_on_all_motors()
 			return self.exit('Standing_Up')
 		else:
 			return
